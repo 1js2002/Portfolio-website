d7e3f7a95f6f16c22b5869b0d9f230994f466425
1js2002 <dgoud99120@gmail.com>
2023-04-13 11:00:10 +0530

    finished front page

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import { FC } from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 
 const Hero: FC = () => {
 	const [text, count] = useTypewriter({
-		words: ["Developer", "Designer", "Creator"],
+		words: ["Developer", "Designer", "Content-Writer"],
 		loop: true,
 		delaySpeed: 2000,
 	});
@@ -12,10 +13,33 @@ const Hero: FC = () => {
 	return (
 		<div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
 			<BackgroundCircles />
-			<h1>
-				<span>{text}</span>
-				<Cursor cursorColor="#f7ab0a" />
-			</h1>
+			<img
+				className="relative rounded-full w-32 h-32 mx-auto object-cover"
+				src="/PFP1.jpg" alt="Profile Picture"
+			/>
+			<div className="z-20">
+				<h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+					Geek-Developer
+				</h2>
+				<h1 className="text-5xl lg:text-6xl font-semibold px-10">
+					<span className="mr-3">{text}</span>
+					<Cursor cursorColor="#f7ab0a" />
+				</h1>
+				<div className="pt-5">
+					<Link href="#about">
+						<button className="heroButton">About</button>
+					</Link>
+					{/* <Link href="#experience">
+						<button className="heroButton">Experience</button>
+					</Link> */}
+					<Link href="#skills">
+						<button className="heroButton">Skills</button>
+					</Link>
+					<Link href="#projects">
+						<button className="heroButton">Projects</button>
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
 	return (
@@ -15,7 +16,9 @@ export default function Home() {
 				<Hero />
 			</section>
 			{/* About */}
-
+				<section>
+					<About />
+				</section>
 			{/* Experience */}
 
 			{/* Skills */}
