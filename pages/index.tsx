85ecf33d643d23e46e51ca41c00bc0b60c701acd
1js2002 <dgoud99120@gmail.com>
2023-04-17 19:26:30 +0530

    building Projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 //import WorkExperience from './components/WorkExperience'
 export default function Home() {
 	return (
@@ -27,7 +28,9 @@ export default function Home() {
 			</section>
 			
 
-			{/* Projects */}
+			<section id="projects" className="snap-start">
+				<Projects />
+			</section>
 
 			{/* Contact Me */}
 		</div>
